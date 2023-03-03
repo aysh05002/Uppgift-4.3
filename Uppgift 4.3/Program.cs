6666996b0ee253fead3a089b45b7ec39a29f7695
Souzan <Souzan@DESKTOP-VHKB7GI>
2023-03-03 22:24:23 +0200

    Add project files.

---
+++ Uppgift 4.3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                Console.WriteLine("FIZZBUZZ");
+            }
+            else if (i % 5 == 0)
+            {
+                Console.WriteLine("BUZZ");
+            }
+            else if (i % 3 == 0)
+            {
+                Console.WriteLine("FIZZ");
+            }
+            else
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
